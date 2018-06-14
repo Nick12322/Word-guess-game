80a9e3c253b27331f1e2f846372dfbcb4701bd04
Nick Woodward <nickwoodward5@gmail.com>
2018-06-14 12:40:11 -0700

    psychic game 99% complete

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,12 @@
 // Set random letters for computer to pick
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-
-console.log(computerGuess);
+function resetGame() {
+    guessesLeft = 7;
+    computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+    userGuessList = [];
+    console.log(computerGuess);
+}
 
 var userGuessList = [];
 
@@ -13,45 +16,42 @@ var wins = 0;
 
 var losses = 0;
 
+var computerGuess = ''
+
 
 
 
 
 // Logic for comparing computer letter to player input
+resetGame();
 
 document.onkeydown = function (event) {
+
 
     var userGuess = event.key;
 
     userGuessList.push(userGuess);
-
+    
     document.getElementById("guesses").innerHTML = userGuessList;
 
-    if (userGuessList.length == 6 ) {
-        losses++;
-        computerGuess;
-        userGuessList = [];
-        guessesLeft == seven;
-    };
-
-    
     if (userGuess != computerGuess) {
         guessesLeft--;
     };
 
-    if (userGuess === computerGuess) {
-        wins++;
-        guessesLeft = 7;
-        userGuessList = [];
-        computerGuess;
+    if (userGuessList.length == 7 ) {
+        resetGame();
+        losses++;
     };
+
+    if (userGuess === computerGuess) {
+        resetGame();
+        wins++;
+    };
+
+
 
     document.getElementById("wins").innerHTML = wins;
     document.getElementById("losses").innerHTML = losses;
     document.getElementById("guessesLeft").innerHTML = guessesLeft;
 
 }
-
-// Show player letter input on screen
-
-// Increase wins or losses, guesses left.
